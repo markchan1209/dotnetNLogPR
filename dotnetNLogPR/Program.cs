a0fc6ea7913f2bf266eff4c5b41b09d9ae1c0b31
markchan1209 <markchan1209@gmail.com>
2020-08-31 11:58:26 +0800

    將專案併入Git

---
+++ dotnetNLogPR/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnetNLogPR
+{
+    class Program
+    {
+        //Trace Debug Info Warn Error Fatal
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+                Logger.Trace("Hello world Trace");
+                Logger.Debug("Hello world Debug");
+                Logger.Info("Hello world Info");
+                Logger.Warn("Hello world Warn");
+                Logger.Error("Hello world Error");
+                Logger.Fatal("Hello world Fatal");
+                Console.ReadKey();
+          
+        }
+    }
+}
